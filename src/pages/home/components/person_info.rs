321b4c71dfb15c289502de6ca5aa50305dfa3f13
xfy <i@rua.plus>
2025-04-28 10:56:00 +0800

    feat(home): add personal info

--- src/pages/home/components/person_info.rs
+++ src/pages/home/components/person_info.rs
@@ -3,5 +3,20 @@ use leptos::prelude::*;
 /// Personal information in left
 #[component]
 pub fn PersonInfo() -> impl IntoView {
-    view! { <div>person info</div> }
+    view! {
+        <div class="flex flex-col items-center text-card-300">
+            <h1 class="text-3xl mb-2 font-semibold">
+                李林军
+            </h1>
+            <div class="mb-1">
+                全栈开发工程师
+            </div>
+            <div class="mb-1">
+                男 / 2000.02
+            </div>
+            <div class="flex rounded-full font-semibold text-[#333] bg-card-200 px-2 w-max">
+                3 年工作经验
+            </div>
+        </div>
+    }
 }
--- src/pages/home/mod.rs
+++ src/pages/home/mod.rs
@@ -9,10 +9,10 @@ pub fn Home() -> impl IntoView {
     view! {
         <Title text="RUA" />
         // main background
-        <main class="dark:bg-rua-gray-900 dark:text-rua-gray-100 h-full flex-column justify-center sm:flex sm:py-8">
-            <div class="shadow-lg w-full sm:max-w-5xl flex-column sm:flex">
+        <main class="dark:bg-rua-gray-900 dark:text-rua-gray-100 h-full flex flex-col sm:justify-center sm:flex-row sm:py-8">
+            <div class="shadow-lg w-full sm:max-w-5xl flex flex-col sm:flex-row">
                 // left 1/3 width on pc
-                <div class="w-full sm:w-1/3 bg-card-100">
+                <div class="w-full sm:w-1/3 bg-card-100 p-8">
                     <PersonInfo />
                 </div>
                 // right 2/3 width on pc
